0203ab47a4661ab9d9d343da89dbe2b5257665e7
RAJEEV SUTRAKAR <sutrakarrajeev22@gmail.com>
2024-12-12 20:42:59 +0530

    add authenication and authorization

--- models/person.js
+++ models/person.js
@@ -1,37 +1,68 @@
-const mongoose=require('mongoose');
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 //define the Person Schema
-const personSchema= new mongoose.Schema({
-    name:{
+const personSchema = new mongoose.Schema({
+    name: {
         type: String,
-        required:true
+        required: true
     },
-    age:{
-        type:Number
+    age: {
+        type: Number
     },
-    work:{
-        type:String,
-        enum:['chef','waiter','manager','HR'],
-        required:true
+    work: {
+        type: String,
+        enum: ['chef', 'waiter', 'manager', 'HR'],
+        required: true
     },
-    mobile:{
-        type:String,
-        required:true
+    mobile: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    address:{
-        type:String
+    address: {
+        type: String
     },
-    salary:{
-        type:Number,
-        required:true
+    salary: {
+        type: Number,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+personSchema.pre('save', async function (next) {
+    const person = this;
+    //hash the password only if it is not present in database
+    if (!person.isModified('password'))
+        return next();
+    try {
+        //hash password generation
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        person.password = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
     }
 })
 
+personSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        const isMatch = bcrypt.compare(candidatePassword, this.password)
+        return isMatch;
+    } catch (error) {
+        throw error;
+    }
+}
 //create Person Model
-const Person=mongoose.model('Person',personSchema);
-module.exports=Person;
+const Person = mongoose.model('Person', personSchema);
+module.exports = Person;
--- server.js
+++ server.js
@@ -2,26 +2,36 @@ const express = require('express');
 const app = express();
 const db = require('./db'); // Ensure `db` initializes your MongoDB connection
 require('dotenv').config();
-
+const passport = require('./auth');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // Parse JSON bodies and store them in req.body
-const PORT=process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
+
+// Middleware Function
+const logRequest = (req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] Request made to: ${req.originalUrl}`);
+  next(); // move on to the next phase
+};
+
+app.use(logRequest);
+
+app.use(passport.initialize());
 
 
-// Welcome route
+const localmiddleware = passport.authenticate('local', { session: false });
 app.get('/', (req, res) => {
   res.send('Welcome to my hotel... How can I help you...sir?');
 });
 
 
 //import the router files 
-const personRoutes=require('./routes/personRoutes');
-const menuRoutes=require('./routes/menuRoutes');
+const personRoutes = require('./routes/personRoutes');
+const menuRoutes = require('./routes/menuRoutes');
+const Person = require('./models/person');
 
 //use the routers
-app.use('/person',personRoutes);
-app.use('/menu',menuRoutes);
-
+app.use('/person', localmiddleware, personRoutes);
+app.use('/menu', menuRoutes);
 
 // Start the server
 app.listen(PORT, () => {
